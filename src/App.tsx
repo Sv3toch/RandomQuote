40ab4213ab0c173c48539cf5b41f5679de4ec710
Sv3toch <svititchs@yandex.by>
2025-08-10 18:13:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import QuoteBox from "./components/QuoteBox.tsx";
+
+function App() {
+
+  return (
+      <div className="App">
+          <h1> Случайная цитата</h1>
+          <QuoteBox />
+      </div>
+  );
+}
+
+export default App
---
+++ src/components/QuoteBox.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+const QuoteBox = () => {
+    const [quotes, setQuotes] = useState([]);
+    const [currentQuote, setCurrentQuote] = useState(null);
+
+    // Загрузка JSON при старте
+    useEffect(() => {
+        fetch('/quotes.json') // если quotes.json в public
+            .then(res => res.json())
+            .then(data => {
+                setQuotes(data);
+                setCurrentQuote(getRandomQuote(data));
+            });
+    }, []);
+
+    const getRandomQuote = (data:SetStateAction<null>) => {
+        const index = Math.floor(Math.random() * data.length);
+        return data[index];
+    };
+
+    const handleClick = () => {
+        setCurrentQuote(getRandomQuote(quotes));
+    };
+
+    return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+            {currentQuote && (
+                <>
+                    <p style={{ fontSize: '1.5rem', fontStyle: 'italic' }}>
+                        “{currentQuote.quote}”
+                    </p>
+                    <p style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+                        — {currentQuote.author}
+                    </p>
+                </>
+            )}
+            <button onClick={handleClick} style={{ marginTop: '2rem' }}>
+                Показать другую цитату
+            </button>
+        </div>
+    );
+};
+
+export default QuoteBox;
